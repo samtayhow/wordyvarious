042164f99b13bd1d6d94b18104c79cca8b3774bb
samtayhow <hello@taylorontheweb.com>
2018-10-25 05:23:42 +0000

    uploaded list play view

---
+++ resources/views/lists.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('main')
+
+    <div class="container">
+        <div id="ShowLists"></div>   
+    </div>
+
+@endsection
---
+++ resources/views/playlist.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('main')
+
+    <div id="playListWrapper"></div>
+    
+@endsection
